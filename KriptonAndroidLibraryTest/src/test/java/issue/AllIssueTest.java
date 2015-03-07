18011b2a6286ffcfd64758dc638ce86ddfe9d506
xcesco <abubusoft@gmail.com>
2015-03-07 03:49:27 +0100

    KRIPTON-4: Test final and static
    Task-Url: http://xsvn:8080/jira/browse/KRIPTON-4

--- KriptonAndroidLibraryTest/src/test/java/issue/AllIssueTest.java
+++ KriptonAndroidLibraryTest/src/test/java/issue/AllIssueTest.java
@@ -10,6 +10,9 @@ import issue.kripton_1.Issue1Test3;
 import issue.kripton_1.Issue1Test4;
 import issue.kripton_1.Issue1Test5;
 import issue.kripton_3.Issue3Test0;
+import issue.kripton_4.Issue4Test0;
+import issue.kripton_4.Issue4Test1;
+import issue.kripton_4.Issue4Test2;
 import issue.kripton_6.Issue6Test0;
 
 import org.junit.runner.RunWith;
@@ -25,6 +28,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
 	Issue1Test0.class, Issue1Test1.class , Issue1Test2.class, Issue1Test3.class, Issue1Test4.class, Issue1Test5.class,
 	Issue3Test0.class,
+	Issue4Test0.class,Issue4Test1.class,Issue4Test2.class,
 	Issue6Test0.class
 	})
 //@formatter:on
