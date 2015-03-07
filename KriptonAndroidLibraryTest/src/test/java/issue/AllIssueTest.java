198c6cd1fdd288c3ee2aa6a0b6e1f3e025b77739
xcesco <abubusoft@gmail.com>
2015-03-07 04:05:17 +0100

    KRIPTON-7: Test List and ArrayList
    Task-Url: http://xsvn:8080/jira/browse/KRIPTON-7

--- KriptonAndroidLibraryTest/src/test/java/issue/AllIssueTest.java
+++ KriptonAndroidLibraryTest/src/test/java/issue/AllIssueTest.java
@@ -14,6 +14,12 @@ import issue.kripton_4.Issue4Test0;
 import issue.kripton_4.Issue4Test1;
 import issue.kripton_4.Issue4Test2;
 import issue.kripton_6.Issue6Test0;
+import issue.kripton_7.Issue7Test0;
+import issue.kripton_7.Issue7Test1;
+import issue.kripton_7.Issue7Test2;
+import issue.kripton_7.Issue7Test3;
+import issue.kripton_7.Issue7Test4;
+import issue.kripton_7.Issue7Test5;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -29,7 +35,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	Issue1Test0.class, Issue1Test1.class , Issue1Test2.class, Issue1Test3.class, Issue1Test4.class, Issue1Test5.class,
 	Issue3Test0.class,
 	Issue4Test0.class,Issue4Test1.class,Issue4Test2.class,
-	Issue6Test0.class
+	Issue6Test0.class,
+	Issue7Test0.class, Issue7Test1.class , Issue7Test2.class, Issue7Test3.class, Issue7Test4.class, Issue7Test5.class,
 	})
 //@formatter:on
 public class AllIssueTest {
---
+++ KriptonAndroidLibraryTest/src/test/java/issue/kripton_7/Bean4.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package issue.kripton_7;
+
+import java.util.Date;
+import java.util.List;
+
+import com.abubusoft.kripton.annotation.BindElement;
+import com.abubusoft.kripton.annotation.BindRoot;
+
+
+
+/**
+ * @author xcesco
+ *
+ */
+@BindRoot
+public class Bean4 extends Bean0 {
+
+	@BindElement
+	private Date birthday;
+	
+	@BindElement
+	private String name;
+
+	@BindElement
+	private String surname;
+	
+	@BindElement List<Integer> tickets;
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+}
