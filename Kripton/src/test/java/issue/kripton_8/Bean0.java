e4c9673753c4a40fefc721b2a78d9ef2c3e8954d
xcesco <abubusoft@gmail.com>
2015-03-09 02:29:58 +0100

    KRIPTON-8: Test BinderValue annotation
    Task-Url: http://xsvn:8080/jira/browse/KRIPTON-8

---
+++ Kripton/src/test/java/issue/kripton_8/Bean0.java
@@ -0,0 +1,9 @@
+package issue.kripton_8;
+
+import com.abubusoft.kripton.annotation.BindRoot;
+
+@BindRoot
+public class Bean0 {
+
+	String bla="Bean0";
+}
---
+++ Kripton/src/test/java/issue/kripton_8/Bean1.java
@@ -0,0 +1,86 @@
+package issue.kripton_8;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URL;
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.xml.namespace.QName;
+
+import com.abubusoft.kripton.annotation.BindRoot;
+import com.abubusoft.kripton.annotation.BindValue;
+
+@BindRoot
+public class Bean1 extends Bean0 {
+
+	public enum BeanType 
+	{
+		TEST_1,
+		TEST_2;
+	}
+	
+	@BindValue
+	BeanType fieldEnum;
+	
+	@BindValue
+	Locale fieldLocale;
+	
+	@BindValue
+	QName fieldQName;
+	
+	@BindValue
+	Calendar fieldCalendar;
+	
+	@BindValue
+	Character fieldChar;
+	
+	@BindValue
+	Date fieldDate;
+	
+	@BindValue
+	BigDecimal fieldBigDecimal;
+	
+	@BindValue
+	URL fieldUrl;
+	
+	@BindValue
+	Boolean fieldBoolean;
+	
+	@BindValue
+	Byte fieldByte;
+	
+	@BindValue
+	Currency fieldCurrency;
+	
+	@BindValue
+	Double fieldDouble;
+	
+	@BindValue
+	Float fieldFloat;
+	
+	@BindValue
+	Long fieldLong;
+	
+	@BindValue
+	Short fieldShort;
+	
+	@BindValue
+	Time fieldTime;
+	
+	@BindValue
+	TimeZone fieldTimeZone;
+	
+	@BindValue
+	Integer fieldInteger;
+	
+	@BindValue
+	String fieldString;
+
+	@BindValue
+	public BigInteger fieldBigInteger;
+}
