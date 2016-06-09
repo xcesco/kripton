8d302658016ad8f7f93a80fe9e98b8cfa1d24eec
xcesco <abubusoft@gmail.com>
2016-06-09 18:05:26 +0200

    KRIPTON-31: Javadoc for @BindDao and @BindSelect, @BindInsert, @BindUpdate, @BindDelete
    Task-Url: http://xsvn:8080/jira/browse/KRIPTON-31

--- KriptonAndroidLibrary/src/com/abubusoft/kripton/android/annotation/BindDao.java
+++ KriptonAndroidLibrary/src/com/abubusoft/kripton/android/annotation/BindDao.java
@@ -8,10 +8,20 @@ import java.lang.annotation.Target;
 import com.abubusoft.kripton.annotation.BindType;
 
 /**
- * <p>This annotation mark an interface which define dao methods for specific bean associated with {@link #value()} attribute.</p>
+ * This annotation mark an interface for DAO (Data Access Object) associated to a specific bean associated with {@link #value()} attribute. 
+ * For each defined methods in the associated interface will be generated an method which use SQL statement. The kind of SQL used in methods will
+ * depends from annotation used to mark method.
+ * 
+ * <p>
+ * Supported query is:
+ * <ul>
+ * 	<li><code>INSERT</code>: with {@link BindInsert} annotation</li>
+ *  <li><code>UPDATE</code></li>
+ *  <li><code>SELECT</code></li>
+ *  <li><code>DELETE</code></li>
+ * </ul> 
  * 
  * <p>Referred {@link #value()} bean must be annotated with {@link BindType} annotation.</p>
- * 
  * 
  * @author xcesco
  *
--- KriptonAndroidLibrary/src/com/abubusoft/kripton/android/annotation/BindInsert.java
+++ KriptonAndroidLibrary/src/com/abubusoft/kripton/android/annotation/BindInsert.java
@@ -6,7 +6,77 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Allow to insert a bean into database.
+ * Allow to insert a bean into database. You can use bean as input parameter or method parameters like bean property, but you can not use mixed case.
+ * 
+ * <p>
+ * For example suppose we want to persist bean <code>Person</code> defined as follow:
+ * 
+ * <pre>
+ * public class Person {
+ *  public long id;
+ *  
+ *  public String name;
+ *  
+ *  public String surname;
+ *  
+ *  public String birthCity;
+ *  
+ *  public Date birthDay;
+ * }
+ * </pre>
+ * 
+ * <p>
+ * The associated DAO interface is
+ * </p>
+ * 
+ * <pre>
+ * &#064;BindDao(Person.class)
+ * public interface PersonDAO {
+ * }
+ * </pre>
+ * 
+ * <h2>Case 1 - Method use a bean type parameter</h2>
+ * 
+ * <p>
+ * It's possible define a INSERT query with annotation {@link BindInsert}. It is possibile to define query parameter simply using method parameter with same name of the bean property.
+ * </p>
+ * 
+ * <pre>
+ * &#064;BindDao(Person.class)
+ * public interface PersonDAO {
+ * 
+ *  &#064;BindInsert
+ *  void insertOne(String name, String surname, String birthCity, Date birthDay);
+ * 
+ *  &#064;BindInsert
+ *  long insertTwo(String name, String surname, String birthCity, Date birthDay);
+ * }
+ * </pre>
+ * 
+ * <p>
+ * Each method parameter will be use like input parameter for query. The name of parameters will be used to map field bean and then the column name of the associated table. If you specify a return type for methods (like method
+ * <code>insertTwo</code>), it has to be of type <code>int, long, Integer, Long</code>. In this case, the return value will be the id value of just inserted row.
+ * 
+ * <h2>Case 2 - method use its parameters like bean properties</h2>
+ * 
+ * <p>
+ * The other way to define an INSERT query is using a bean as input parameter:
+ * 
+ * <pre>
+ * &#064;BindDao(Person.class)
+ * public interface PersonDAO
+ * {
+ * 
+ *  &#064;BindInsert(excludedFields={"name", "surname"}
+ *  void insert(Person bean);
+ * }
+ * </pre>
+ * 
+ * <p>
+ * You can use attribute <b>value</b> to define which property insert into query or you can use attribute <b>excludedFields</b> to avoid to insert some fields,
+ * <b>but you can not use both attributes in the same method definition</b>. Values of this attribute will be used like bean property name. At the end of the insert, bean will have id value set to last row id used to insert bean into
+ * table. If you specify a return type for methods, it has to be of type <code>int, long, Integer, Long</code> and it will contains same value like row id.
+ * 
  * 
  * @author xcesco
  *
@@ -14,19 +84,20 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
 public @interface BindInsert {
-	
+
 	/**
-	 * properties to include into INSERT command
-	 * @return
-	 * 		property's names to include
+	 * <p>
+	 * bean properties to include into INSERT command. <b>To use only if method have only one parameter and its type is the same of supported bean</b>.
+	 * 
+	 * @return property's names to include
 	 */
 	String[] value() default {};
-	
+
 	/**
-	 * properties to include into INSERT command
-	 * @return
-	 * 		property's names to exclude
+	 * properties to exclude into INSERT command. <b>To use only if method have only one parameter and its type is the same of supported bean</b>.
+	 * 
+	 * @return property's names to exclude
 	 */
 	String[] excludedFields() default {};
-	
+
 }
