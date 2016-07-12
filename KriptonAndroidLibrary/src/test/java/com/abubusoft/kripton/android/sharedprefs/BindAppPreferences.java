67ae135e7a270f3411e68565b5b0e57f86342171
xcesco2 <xcesco@gmail.com>
2016-07-12 09:58:47 +0200

    work in progress

--- KriptonAndroidLibrary/src/test/java/com/abubusoft/kripton/android/sharedprefs/BindAppPreferences.java
+++ KriptonAndroidLibrary/src/test/java/com/abubusoft/kripton/android/sharedprefs/BindAppPreferences.java
@@ -6,62 +6,117 @@ import com.abubusoft.kripton.android.KriptonLibrary;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 
 public class BindAppPreferences {
 	
+	private DefaultConverter defaultConverter;
+
 	public BindAppPreferences()
 	{
-		 converterMap = new HashMap<String, Converter>();
+		prefs=KriptonLibrary.context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+		
+		converterMap = new HashMap<String, Converter>();
 		 
-		 DefaultConverter defaultConverter = new DefaultConverter();
+		defaultConverter = new DefaultConverter();
 		 
 		 //converterMap.put("name", defaultConverter);
 	}
-		
-	
+			
 	public class Editor
 	{
+		protected Editor()
+		{
+			edit=prefs.edit();
+		}
+		
+		private android.content.SharedPreferences.Editor edit;
+		
 		public Editor putName(String value)
 		{
-			name=value;
+			edit.putString("name", (String) BindAppPreferences.this.beanToPref("name", bean.name));
 			
 			return this;
 		}
 		
-		public void save()
+		public Editor clearName()
 		{
-			SharedPreferences prefs=KriptonLibrary.context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+			edit.remove("name");
 			
-			android.content.SharedPreferences.Editor edit=prefs.edit();
-			edit.putString("name", (String) BindAppPreferences.this.beanToPref("name", BindAppPreferences.this.name));
-			edit.apply();
+			return this;
 		}
+		
+		public Editor put(AppPreferences bean)
+		{
+			putName(bean.name);
+			
+			return this;
+		}
+		
+		public Editor clear()
+		{
+			edit.clear();
+			
+			return this;
+		}
+		
+		public Editor commit()
+		{
+			edit.commit();
+			
+			return this;
+		}
+		
+		
 	}
 		
-	protected String name;
+	protected AppPreferences bean;
 	
 	protected HashMap<String, Converter> converterMap;
+
+	private SharedPreferences prefs;
 	
 	public static final String SHARED_PREFERENCE_NAME="dummy";
 	
 	public static BindAppPreferences instance()
 	{		
-		SharedPreferences prefs=KriptonLibrary.context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
-		
-		BindAppPreferences bean=new BindAppPreferences();
-		
-		bean.name= bean.prefToBean("name", prefs.getString("name", "default"));
+		BindAppPreferences result=new BindAppPreferences();
+				
+		//bean.name= bean.prefToBean("name", prefs.getString("name", "default"));
 		//prefs.getBoolean(key, defValue)
 		//prefs.getFloat(key, defValue)
 		//prefs.getInt(key, defValue)
 		//prefs.getLong(key, defValue)
 		//prefs.getString(key, defValue)		
+		return result;
+	}
+	
+	public AppPreferences read()
+	{
+		prefs=KriptonLibrary.context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+		AppPreferences bean=new AppPreferences();			
+		
+		bean.name=prefToBean("name", prefs.getString("name", "default"));
+		
 		return bean;
 	}
 	
-	public String getName()
+	public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener)
 	{
-		return name;
+		prefs.registerOnSharedPreferenceChangeListener(listener);
+	}
+	
+	public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener)
+	{
+		prefs.unregisterOnSharedPreferenceChangeListener(listener);
+	}
+	
+	public void write(AppPreferences bean)
+	{
+		Editor edit=this.edit();
+		edit.put(bean);
+		
+		edit.commit();
 	}
 	
 	public Editor edit()
@@ -74,6 +129,9 @@ public class BindAppPreferences {
 		T result=null;
 		if (converterMap.containsKey(key)){
 			value=converterMap.get(key).convertToPreference(key, value);
+		} else 
+		{
+			value=defaultConverter.convertToPreference(
 		}
 		
 		return result;
