35a0b936d52218980417904d575af93291fc6390
xcesco <abubusoft@gmail.com>
2016-10-14 18:37:41 +0200

    KRIPTON-55: Add Upgrade and Downgrade procedure
    Task-Url: http://xsvn:8080/jira/browse/KRIPTON-55

--- KriptonAndroidLibrary/src/main/java/com/abubusoft/kripton/android/sqlite/AbstractDataSource.java
+++ KriptonAndroidLibrary/src/main/java/com/abubusoft/kripton/android/sqlite/AbstractDataSource.java
@@ -22,70 +22,63 @@ import android.database.sqlite.SQLiteOpenHelper;
 public abstract class AbstractDataSource extends SQLiteOpenHelper implements AutoCloseable {
 
 	/**
+	 * Interface for database transactions.
+	 * 
+	 * @author xcesco
+	 *
+	 * @param <E>
+	 */
+	public interface AbstractTransaction<E extends BindDaoFactory> {
+		void onError(Throwable e);
+
+		/**
+		 * Execute transaction. Connection is managed from DataSource
+		 * 
+		 * @param daoFactory
+		 * @return true to commit, false to rollback
+		 */
+		boolean onExecute(E daoFactory);
+	}
+	
+	/**
+	 * Manage upgrade or downgrade of database.
+	 * 
+	 * @author xcesco
+	 *
+	 */
+	public interface OnDatabaseUpdateListener {
+	
+		/**
+		 * <p>Method for DDL or DML
+		 * 
+		 * @param db
+		 * 		database 
+		 * @param oldVersion
+		 * 		current version of database 
+		 * @param newVersion
+		 * 		new version of database
+		 * @param upgrade
+		 * 		if true is an upgrade operation, otherwise it's a download operation.
+		 */
+		void onUpdate(SQLiteDatabase db, int oldVersion, int newVersion, boolean upgrade);
+	}
+	
+	/**
 	 * database instance
 	 */
 	protected SQLiteDatabase database;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.database.sqlite.SQLiteOpenHelper#getReadableDatabase()
-	 */
-	@Override
-	public SQLiteDatabase getReadableDatabase() {
-		return openReadOnlyDatabase();
-	}
-
 	/**
-	 * <p>
-	 * Open a read only database.
-	 * </p>
-	 * 
-	 * @return read only database
+	 * listener to execute code during database upgrade events
 	 */
-	public SQLiteDatabase openReadOnlyDatabase() {
-		if (openCounter.incrementAndGet() == 1) {
-			// open new read database
-			database = super.getReadableDatabase();
-		}
-		return database;
-	}
-
-	/**
-	 * Open database. Is alias for openWritableDatabase()
-	 * 
-	 * @return writable database
-	 */
-	public SQLiteDatabase openDatabase() {
-		return getWritableDatabase();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.database.sqlite.SQLiteOpenHelper#getWritableDatabase()
-	 */
-	@Override
-	public SQLiteDatabase getWritableDatabase() {
-		return openWritableDatabase();
-	}
-
-	/**
-	 * <p>
-	 * open a writeable database.
-	 * </p>
-	 * 
-	 * @return
-	 */
-	public SQLiteDatabase openWritableDatabase() {
-		if (openCounter.incrementAndGet() == 1) {
-			// open new write database
-			database = super.getWritableDatabase();
-		}
-		return database;
-	}
+	protected OnDatabaseUpdateListener databaseUpdateListener;
 
 	private AtomicInteger openCounter = new AtomicInteger();
+
+	/**
+	 * if true, database was update during this application run
+	 */
+	protected boolean upgradedVersion;
 
 	public AbstractDataSource(Context context, String name, CursorFactory factory, int version) {
 		super(context, name, factory, version);
@@ -109,6 +102,26 @@ public abstract class AbstractDataSource extends SQLiteOpenHelper implements Aut
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.database.sqlite.SQLiteOpenHelper#getReadableDatabase()
+	 */
+	@Override
+	public SQLiteDatabase getReadableDatabase() {
+		return openReadOnlyDatabase();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.database.sqlite.SQLiteOpenHelper#getWritableDatabase()
+	 */
+	@Override
+	public SQLiteDatabase getWritableDatabase() {
+		return openWritableDatabase();
+	}
+
 	/**
 	 * <p>
 	 * return true if database is already opened.
@@ -121,22 +134,85 @@ public abstract class AbstractDataSource extends SQLiteOpenHelper implements Aut
 	}
 
 	/**
-	 * Interface for database transactions.
-	 * 
-	 * @author xcesco
-	 *
-	 * @param <E>
+	 * @return the upgradedVersion
 	 */
-	public interface AbstractTransaction<E extends BindDaoFactory> {
-		/**
-		 * Execute transaction. Connection is managed from DataSource
-		 * 
-		 * @param daoFactory
-		 * @return true to commit, false to rollback
-		 */
-		boolean onExecute(E daoFactory);
+	public boolean isUpgradedVersion() {
+		return upgradedVersion;
+	}
 
-		void onError(Throwable e);
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onDowngrade(android.database.sqlite.SQLiteDatabase, int, int)
+	 */
+	@Override
+	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		if (databaseUpdateListener!=null)
+		{
+			databaseUpdateListener.onUpdate(db, oldVersion, newVersion, false);
+			upgradedVersion=true;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		if (databaseUpdateListener!=null)
+		{
+			databaseUpdateListener.onUpdate(db, oldVersion, newVersion, true);
+			upgradedVersion=true;
+		}
+	}
+
+	/**
+	 * Open database. Is alias for openWritableDatabase()
+	 * 
+	 * @return writable database
+	 */
+	public SQLiteDatabase openDatabase() {
+		return getWritableDatabase();
+	}
+	
+	/**
+	 * <p>
+	 * Open a read only database.
+	 * </p>
+	 * 
+	 * @return read only database
+	 */
+	public SQLiteDatabase openReadOnlyDatabase() {
+		if (openCounter.incrementAndGet() == 1) {
+			// open new read database
+			database = super.getReadableDatabase();
+		}
+		return database;
+	}
+
+	/**
+	 * <p>
+	 * open a writable database.
+	 * </p>
+	 * 
+	 * @return
+	 * 		writable database
+	 */
+	public SQLiteDatabase openWritableDatabase() {
+		if (openCounter.incrementAndGet() == 1) {
+			// open new write database
+			database = super.getWritableDatabase();
+		}
+		return database;
+	}
+	
+	/**
+	 * Register the listener. It need to be called before <strong>any</strong> database operations.
+	 * 
+	 * @param listener
+	 * 			listener to user
+	 */
+	public void setOnDatabaseUpdateListener(OnDatabaseUpdateListener listener)
+	{
+		this.databaseUpdateListener=listener;
 	}
 
 }
