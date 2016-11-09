100b2f42a9a11de65b37142335465de84b2cb9e0
xcesco <abubusoft@gmail.com>
2016-11-10 00:07:35 +0100

    KRIPTON-64: Add @BindSqlParam
    Task-Url: http://xsvn:8080/jira/browse/KRIPTON-64

---
+++ KriptonProcessor/src/test/expected/com/abubusoft/kripton/processor/kripton49/entities/Bean01EntityTable.java
@@ -0,0 +1,49 @@
+package com.abubusoft.kripton.processor.kripton49.entities;
+
+import java.lang.String;
+
+/**
+ * <p>
+ * Entity <code>Bean01Entity</code> is associated to table <code>bean01</code>
+ * This class represents table associated to entity.
+ * </p>
+ *  @see Bean01Entity
+ */
+public class Bean01EntityTable {
+  /**
+   * Costant represents name of table bean01
+   */
+  public static final String TABLE_NAME = "bean01";
+
+  /**
+   * <p>
+   * DDL to create table bean01
+   * </p>
+   *
+   * <pre>CREATE TABLE bean01 (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT);</pre>
+   */
+  public static final String CREATE_TABLE_SQL = "CREATE TABLE bean01 (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT);";
+
+  /**
+   * <p>
+   * DDL to drop table bean01
+   * </p>
+   *
+   * <pre>DROP TABLE IF EXISTS bean01;</pre>
+   */
+  public static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS bean01;";
+
+  /**
+   * Entity's property <code>id</code> is associated to table column <code>id</code>. This costant represents column name.
+   *
+   *  @see Bean01Entity#id
+   */
+  public static final String COLUMN_ID = "id";
+
+  /**
+   * Entity's property <code>text</code> is associated to table column <code>text</code>. This costant represents column name.
+   *
+   *  @see Bean01Entity#text
+   */
+  public static final String COLUMN_TEXT = "text";
+}
