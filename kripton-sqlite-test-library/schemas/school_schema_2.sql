c49c19db5664828d7771d083469ea8b279dfb81a
xcesco <abubusoft@gmail.com>
2019-02-19 01:31:35 +0100

    @wip

--- kripton-sqlite-test-library/schemas/school_schema_2.sql
+++ kripton-sqlite-test-library/schemas/school_schema_2.sql
@@ -2,13 +2,11 @@
 --
 -- Filename: school_schema_2.sql
 --
--- Date: Wed Aug 15 01:21:54 CEST 2018
+-- Date: Tue Feb 19 00:30:47 CET 2019
 --
 ------------------------------------------------------------------------------------
 
 CREATE TABLE seminar (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, location TEXT, name TEXT);
 CREATE TABLE student (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, location TEXT, name TEXT);
-CREATE TABLE seminar_2_student (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, seminar_id INTEGER, student_id INTEGER, FOREIGN KEY(seminar_id) REFERENCES seminar(id), FOREIGN KEY(student_id) REFERENCES student(id)); 
-CREATE TABLE professor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, birth_date TEXT, name TEXT, surname TEXT NOT NULL);
-CREATE UNIQUE INDEX idx_seminar_2_student_0 on seminar_2_student (student_id asc,  seminar_id desc);
-CREATE INDEX idx_professor_0 on professor (surname);
+CREATE TABLE seminar_2_student (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, seminar_id INTEGER, student_id INTEGER, FOREIGN KEY(seminar_id) REFERENCES seminar(id), FOREIGN KEY(student_id) REFERENCES student(id), UNIQUE (student_id, seminar_id)); CREATE UNIQUE INDEX idx_seminar_2_student_0 on seminar_2_student (student_id asc,  seminar_id desc);
+CREATE TABLE professor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, birth_date TEXT, name TEXT, surname TEXT NOT NULL); CREATE INDEX idx_professor_0 on professor (surname);
