de4cdfe1c7a02aaba42288b449b5f2152d30de27
Francesco Benincasa <xcesco@gmail.com>
2019-10-22 17:42:09 +0200

    @wip

--- kripton-orm/src/main/java/com/abubusoft/kripton/android/sqlite/FactoryDataSource.java
+++
@@ -1,8 +0,0 @@
-package com.abubusoft.kripton.android.sqlite;
-
-public class FactoryDataSource {
-
-	/*public <E extends AbstractDataSource> create(Class<E> clazz, byte[] secret) {
-		return null;
-	}*/
-}
