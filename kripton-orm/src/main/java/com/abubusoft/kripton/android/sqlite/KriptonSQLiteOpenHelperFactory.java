a3fd642e1c31730d3861e332821e61728eca6875
xcesco <xcesco@gmail.com>
2019-10-23 12:03:30 +0200

    @wip

---
+++ kripton-orm/src/main/java/com/abubusoft/kripton/android/sqlite/KriptonSQLiteOpenHelperFactory.java
@@ -0,0 +1,14 @@
+package com.abubusoft.kripton.android.sqlite;
+
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Configuration;
+
+public class KriptonSQLiteOpenHelperFactory implements SupportSQLiteOpenHelper.Factory {
+
+	@Override
+	public SupportSQLiteOpenHelper create(Configuration configuration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
